604a141036151a767c079443f1d1f538a3aad43e
Devin Sweeting <devinsweeting@gmail.com>
2018-12-14 10:31:31 -0800

    initial commit, set up prohect, set up products database, set up first view

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,8 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+    render :index
+  end
+  
+end
---
+++ db/migrate/20181214180107_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t. column :strain, :string
+      t. column :cann_abbr, :string
+      t. column :cost, :float
+      t. column :origin, :string
+
+      t. timestamps
+    end
+  end
+end
