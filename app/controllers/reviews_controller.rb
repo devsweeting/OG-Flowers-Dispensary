ba6e07c6f506bed7cd4ff3f9419f23c8f33fdd84
Devin Sweeting <devinsweeting@gmail.com>
2018-12-14 14:20:11 -0800

    try functionailty to add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,9 +5,16 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.new
   end
 
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Course.find(params[:id])
+    redirect_to products_path(@review)
+  end
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
+    binding.pry
     if @review.save
       redirect_to product_path(@review.product)
     else
