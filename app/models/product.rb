7e992023832e499b1490024161ce2382d2f6b6e2
Devin Sweeting <devinsweeting@gmail.com>
2018-12-16 15:28:36 -0800

    Add styling, add commented out messages for scope and recent call backs to model/**.rb files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  scope :ten_most_recent, -> { order(created_at: :desc).limit(10)}
   has_many :reviews
   validates :strain, :presence => true
   validates :cann_abbr, :presence => true
